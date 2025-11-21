631f5b3cae2e88a199b096dd0011de4d129b2c74
elianAVG00 <elian.avgonzalez@hotmail.com>
2025-11-21 12:03:23 -0300

    agrego fix authProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,12 +5,15 @@ import { router } from "@/app/routes";
 import "@/index.css";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "@/contexts/ThemeContext";
+import { AuthProvider } from "@/features/auth/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <RouterProvider router={router} />
-      <Toaster richColors position="top-right" />
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider>
+        <RouterProvider router={router} />
+        <Toaster richColors position="top-right" />
+      </ThemeProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
